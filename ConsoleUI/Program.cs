5af513dedc8f914ff1af934bceae088c79805d4c
iHSAN ARVAS <408473@ogr.ktu.edu.tr>
2021-02-03 17:08:36 +0300

    a

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+using Bussiness.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetlAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+
+    
+}
